fbc781cdd9fdde35a64ecf39db29b8651508c185
Tarocch1 <huiben.fang@gmail.com>
2021-02-28 16:16:09 +0800

    添加 Main Workflow

---
+++ .github/actions/main/deploy.sh
@@ -0,0 +1,11 @@
+cp -r tmp/.git dist/
+cd build
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git status
+git add -A
+if [ $? -eq 0 ]
+then
+  git commit -m "Deploy by Github Actions"
+  git push
+fi
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Tarocch1's Home Page
+# Tarocch1's Home Page ![Main Workflow](https://github.com/Tarocch1/tarocch1.github.io/workflows/Main%20Workflow/badge.svg)
