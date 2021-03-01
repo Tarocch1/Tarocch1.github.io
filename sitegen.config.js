ab2308900346f57aea24a2c2362b01d8e485490e
Tarocch1 <huiben.fang@gmail.com>
2021-03-02 00:17:18 +0800

    设置时区

--- sitegen.config.js
+++ sitegen.config.js
@@ -1,5 +1,16 @@
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 module.exports = {
-  title: 'Tarocch1\'s Home Page',
+  title: "Tarocch1's Home Page",
+  markdown: {
+    timeFormatter: time =>
+      dayjs(time).tz('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss'),
+  },
   build: {
     dist: '../dist',
   },
